e7917c31017b2dd176023249e4a6c5362a5ad02a
SharmanJava <kirtisjeurkar@gmail.com>
2021-05-25 16:28:06 +0530

    Basic TODO tasks

---
+++ src/com/cafe/order/Main.java
@@ -0,0 +1,13 @@
+package com.cafe.order;
+
+public class Main {
+
+    public static void main(String[] args) {
+    /*
+    TODO Ask the number of member visiting in variable customerNumber
+    TODO menu option for the customers
+    TODO Use arrays to store the order
+    TODO Show the total bill .......   (optional)
+    */
+    }
+}
