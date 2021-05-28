e62b14a30e66ccbcbacf7424e7b3dabb4e771b2d
SharmanJava <kirtisjeurkar@gmail.com>
2021-05-28 17:37:21 +0530

    Dynamical array used for storing user data.

--- src/com/cafe/order/Main.java
+++ src/com/cafe/order/Main.java
@@ -13,8 +13,30 @@ public class Main {
      */
 
     public static void main(String[] args) {
-
+        String option;
+        boolean cont;
+        Scanner sc = new Scanner(System.in);
         MenuCard mc = new MenuCard();
         mc.menuCard();
+        TakeOrder to = new TakeOrder();
+        System.out.println("Please give your order from above items");
+        do {
+            String c_order = sc.next();
+            to.push(c_order);
+            System.out.println("Do you want anything more?");
+            option = sc.next();
+            if (option.equalsIgnoreCase("yes")) {
+
+                System.out.println("Next....");
+            } else {
+                System.out.println("Sir final bill processing......");
+                System.out.println();
+                to.show();
+                break;
+            }
+
+        } while (cont = true);
+
+
     }
 }
