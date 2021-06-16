da3d94551b81f876f781024461c4d680a11e4671
Sharman Jeurkar <72147825+SharmanJeurkar@users.noreply.github.com>
2021-06-16 17:37:23 +0530

    Delete CafeOrderMahineCL directory

--- CafeOrderMahineCL/src/com/cafe/order/ShowArray.java
+++
@@ -1,9 +0,0 @@
-package com.cafe.order;
-
-public class ShowArray {
-    public void showArray(Object[] obj) {
-        for (Object obj1 : obj)
-            System.out.print(obj1 + " ");
-        System.out.println();
-    }
-}
