f075e2bb8d932dd0b9768c0d880207fb0a3c4bf2
Sharman Jeurkar <72147825+SharmanJeurkar@users.noreply.github.com>
2021-06-25 13:06:22 +0530

    Codacy errors in class solved

--- CafeOrderingMachineWeb/src/main/java/com/cafe/dao/Connector.java
+++ CafeOrderingMachineWeb/src/main/java/com/cafe/dao/Connector.java
@@ -4,8 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Connector {
-	Connection conn=null;
-    public static Connection Connect(){
+	
+    public static Connection connect(){
         String url="jdbc:mysql://localhost:3306/CafeOrderWeb";
         String user="Sharman";
         String password="1234";
