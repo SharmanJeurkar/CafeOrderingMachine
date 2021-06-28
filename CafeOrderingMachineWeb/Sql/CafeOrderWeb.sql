89eb16c800e01f05481b6dd2b7444adfd58e60f9
Sharman Jeurkar <72147825+SharmanJeurkar@users.noreply.github.com>
2021-06-28 09:36:46 +0530

    Create CafeOrderWeb.sql

---
+++ CafeOrderingMachineWeb/Sql/CafeOrderWeb.sql
@@ -0,0 +1,26 @@
+create schema CafeOrderWeb;
+use CafeOrderWeb;
+
+create table Beverages(Item_Name varchar(20), Cost int);
+create table Snacks(Item_Name varchar(20), Cost int);
+create table Staretrs(Item_Name varchar(20), Cost int);
+create table Main_Menu(Item_Name varchar(20), Cost int);
+create table Rice(Item_Name varchar(20), Cost int);
+
+insert into Beverages values("Tea",  15);
+insert into Beverages values("Coffee",  25);
+
+insert into Snacks values("Dosa",  45);
+insert into Snacks values("Idli",  30);
+
+insert into Starters values("Tomato Soup",  60);
+insert into Starters values("Manchow Soup",  80);
+
+insert into Main_Menu values("Panner Butter Masala",  100);
+insert into Main_Menu values("Butter Roti",  90);
+
+insert into Rice values("Schezwan Rice",  100);
+insert into Rice values("Jeera Rice",  70);
+
+insert into Desserts values("Gulabjam",  120);
+insert into Desserts values("Rasmali",  130);
