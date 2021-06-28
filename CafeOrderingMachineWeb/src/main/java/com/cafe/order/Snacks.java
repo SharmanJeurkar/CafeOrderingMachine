e56cc368d6bb89cf902a83bbc42d1415596c715b
Sharman Jeurkar <72147825+SharmanJeurkar@users.noreply.github.com>
2021-06-28 20:16:52 +0530

    Update Snacks.java

--- CafeOrderingMachineWeb/src/main/java/com/cafe/order/Snacks.java
+++ CafeOrderingMachineWeb/src/main/java/com/cafe/order/Snacks.java
@@ -25,16 +25,13 @@ public class Snacks extends HttpServlet {
 			String snk_item3=request.getParameter("snk_item3");
 			String snk_item4=request.getParameter("snk_item4");
 			String snk_item5=request.getParameter("snk_item5");
-			int order_number=0;
-			order_number=+1;
 			
-			String query="create table snk_order"+order_number+"(item_1 varchar(20), item_2 varchar(20), item_3 varchar(20), item_4 varchar(20), item_5 varchar(20))";
-			String sql="insert into  snk_order"+order_number+" values('"+snk_item1+"', '"+snk_item2+"', '"+snk_item3+"', '"+snk_item4+"', '"+snk_item5+"')";	
+			String sql="insert into  snk_order values('"+snk_item1+"', '"+snk_item2+"', '"+snk_item3+"', '"+snk_item4+"', '"+snk_item5+"')";	
 			try {
 				Connection con=Connector.connect();
-				PreparedStatement pst=con.prepareStatement(query);
+				
 				PreparedStatement ps=con.prepareStatement(sql);
-				pst.executeUpdate();
+				
 				ps.executeUpdate();
 				RequestDispatcher red=request.getRequestDispatcher("ShowOrder.jsp");
 				
