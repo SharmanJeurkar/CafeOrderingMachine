86bdcd70c28fa6109f0794dec90809a69912bad8
Sharman Jeurkar <72147825+SharmanJeurkar@users.noreply.github.com>
2021-06-29 10:26:17 +0530

    this file will delete the table after assigned time period

---
+++ CafeOrderingMachineWeb/src/main/java/com/cafe/dao/DeleteTableTimer.java
@@ -0,0 +1,128 @@
+package com.cafe.dao;
+
+import java.sql.*;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class DeleteTableTimer {
+	public void beverages() {
+		Timer timer=new Timer();
+		TimerTask task=new TimerTask() {
+			
+			@Override
+			public void run() {
+				try {
+					Connection con=Connector.connect();
+					PreparedStatement pst=con.prepareStatement("drop table bev_order");
+					System.out.println("table dropped.....");
+					pst.executeUpdate();
+				  }catch(Exception e){
+					System.out.println(e);
+				}
+				
+			}
+		};
+		timer.schedule(task, 120000);
+	}
+	
+	public void Snacks() {
+		Timer timer=new Timer();
+		TimerTask task=new TimerTask() {
+			
+			@Override
+			public void run() {
+				try {
+					Connection con=Connector.connect();
+					PreparedStatement pst=con.prepareStatement("drop table snk_order");
+					System.out.println("table dropped.....");
+					pst.executeUpdate();
+				  }catch(Exception e){
+					System.out.println(e);
+				}
+				
+			}
+		};
+		timer.schedule(task, 120000);
+	}
+	
+	public void Stareters() {
+		Timer timer=new Timer();
+		TimerTask task=new TimerTask() {
+			
+			@Override
+			public void run() {
+				try {
+					Connection con=Connector.connect();
+					PreparedStatement pst=con.prepareStatement("drop table str_order");
+					System.out.println("table dropped.....");
+					pst.executeUpdate();
+				  }catch(Exception e){
+					System.out.println(e);
+				}
+				
+			}
+		};
+		timer.schedule(task, 120000);
+	}
+	
+	public void MainMenu() {
+		Timer timer=new Timer();
+		TimerTask task=new TimerTask() {
+			
+			@Override
+			public void run() {
+				try {
+					Connection con=Connector.connect();
+					PreparedStatement pst=con.prepareStatement("drop table mm_order");
+					System.out.println("table dropped.....");
+					pst.executeUpdate();
+				  }catch(Exception e){
+					System.out.println(e);
+				}
+				
+			}
+		};
+		timer.schedule(task, 120000);
+	}
+	
+	public void Rice() {
+		Timer timer=new Timer();
+		TimerTask task=new TimerTask() {
+			
+			@Override
+			public void run() {
+				try {
+					Connection con=Connector.connect();
+					PreparedStatement pst=con.prepareStatement("drop table rice_order");
+					System.out.println("table dropped.....");
+					pst.executeUpdate();
+				  }catch(Exception e){
+					System.out.println(e);
+				}
+				
+			}
+		};
+		timer.schedule(task, 120000);
+	}
+	
+	public void Desserts() {
+		Timer timer=new Timer();
+		TimerTask task=new TimerTask() {
+			
+			@Override
+			public void run() {
+				try {
+					Connection con=Connector.connect();
+					PreparedStatement pst=con.prepareStatement("drop table des_order");
+					System.out.println("table dropped.....");
+					pst.executeUpdate();
+				  }catch(Exception e){
+					System.out.println(e);
+				}
+				
+			}
+		};
+		timer.schedule(task, 120000);
+	}
+	
+}
