21a11ee10b178fe322b8e0fe51d77bf1481c9410
Sharman Jeurkar <72147825+SharmanJeurkar@users.noreply.github.com>
2021-06-30 15:47:12 +0530

    Update Main_menu.java

--- CafeOrderingMachineWeb/src/main/java/com/cafe/order/Main_menu.java
+++ CafeOrderingMachineWeb/src/main/java/com/cafe/order/Main_menu.java
@@ -1,21 +1,23 @@
 package com.cafe.order;
 
 import java.io.IOException;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
-import javax.servlet.RequestDispatcher;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.cafe.dao.Connector;
 
 
-@WebServlet("/MainMenu")
-public class MainMenu extends HttpServlet {
+@WebServlet("/Main_Menu")
+public class Main_Menu extends HttpServlet {
 	private static final long serialVersionUID = 1L;
      
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -26,7 +28,6 @@ public class MainMenu extends HttpServlet {
 		String mm_item5=request.getParameter("mm_item5");
 		
 		
-		
 		String sql="insert into  mm_order values('"+mm_item1+"', '"+mm_item2+"', '"+mm_item3+"', '"+mm_item4+"', '"+mm_item5+"')";	
 		try {
 			Connection con=Connector.connect();
@@ -34,14 +35,17 @@ public class MainMenu extends HttpServlet {
 			PreparedStatement ps=con.prepareStatement(sql);
 			
 			ps.executeUpdate();
-			RequestDispatcher red=request.getRequestDispatcher("ShowOrder.jsp");
 			
-			request.setAttribute("mm_item1", mm_item1);
-			request.setAttribute("mm_item2", mm_item2);
-			request.setAttribute("mm_item3", mm_item3);
-			request.setAttribute("mm_item4", mm_item4);
+			HttpSession session= request.getSession();
+			
+			session.setAttribute("mm_item1", mm_item1);
+			session.setAttribute("mm_item2", mm_item2);
+			session.setAttribute("mm_item3", mm_item3);
+			session.setAttribute("mm_item4", mm_item4);
 			request.setAttribute("mm_item5", mm_item5);
-			red.forward(request, response);
+			
+			response.sendRedirect("Rice.jsp");
+			
 			
 		}catch(Exception e) {
 			System.out.println(e);
