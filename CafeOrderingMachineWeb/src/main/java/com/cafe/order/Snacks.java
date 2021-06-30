fc97fdff5dec2000191e13cc4a958047a325c4cc
Sharman Jeurkar <72147825+SharmanJeurkar@users.noreply.github.com>
2021-06-30 15:45:45 +0530

    Update Snacks.java

--- CafeOrderingMachineWeb/src/main/java/com/cafe/order/Snacks.java
+++ CafeOrderingMachineWeb/src/main/java/com/cafe/order/Snacks.java
@@ -1,15 +1,17 @@
 package com.cafe.order;
 
 import java.io.IOException;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
-import javax.servlet.RequestDispatcher;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.cafe.dao.Connector;
 
@@ -26,21 +28,25 @@ public class Snacks extends HttpServlet {
 			String snk_item4=request.getParameter("snk_item4");
 			String snk_item5=request.getParameter("snk_item5");
 			
+			
+			
+			
 			String sql="insert into  snk_order values('"+snk_item1+"', '"+snk_item2+"', '"+snk_item3+"', '"+snk_item4+"', '"+snk_item5+"')";	
 			try {
 				Connection con=Connector.connect();
 				
 				PreparedStatement ps=con.prepareStatement(sql);
-				
 				ps.executeUpdate();
-				RequestDispatcher red=request.getRequestDispatcher("ShowOrder.jsp");
 				
-				request.setAttribute("snk_item1", snk_item1);
-				request.setAttribute("snk_item2", snk_item2);
-				request.setAttribute("snk_item3", snk_item3);
-				request.setAttribute("snk_item4", snk_item4);
-				request.setAttribute("snk_item5", snk_item5);
-				red.forward(request, response);
+				
+				HttpSession session=request.getSession();
+				session.setAttribute("snk_item1", snk_item1);
+				session.setAttribute("snk_item2", snk_item2);
+				session.setAttribute("snk_item3", snk_item3);
+				session.setAttribute("snk_item4", snk_item4);
+				session.setAttribute("snk_item5", snk_item5);
+				
+				response.sendRedirect("Starters.jsp");
 				
 			}catch(Exception e) {
 				System.out.println(e);
