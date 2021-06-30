591d7d23094e52b051ec0c65c62ea485d231e3a5
Sharman Jeurkar <72147825+SharmanJeurkar@users.noreply.github.com>
2021-06-30 15:48:29 +0530

    Update Rice.java

--- CafeOrderingMachineWeb/src/main/java/com/cafe/order/Rice.java
+++ CafeOrderingMachineWeb/src/main/java/com/cafe/order/Rice.java
@@ -1,15 +1,17 @@
 package com.cafe.order;
 
 import java.io.IOException;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
-import javax.servlet.RequestDispatcher;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.cafe.dao.Connector;
 
@@ -25,6 +27,8 @@ public class Rice extends HttpServlet {
 		String rice_item4=request.getParameter("rice_item4");
 		String rice_item5=request.getParameter("rice_item5");
 		
+		
+		
 		String sql="insert into  rice_order values('"+rice_item1+"', '"+rice_item2+"', '"+rice_item3+"', '"+rice_item4+"', '"+rice_item5+"')";	
 		try {
 			Connection con=Connector.connect();
@@ -32,14 +36,18 @@ public class Rice extends HttpServlet {
 			PreparedStatement ps=con.prepareStatement(sql);
 			
 			ps.executeUpdate();
-			RequestDispatcher red=request.getRequestDispatcher("ShowOrder.jsp");
 			
-			request.setAttribute("rice_item1", rice_item1);
-			request.setAttribute("rice_item2", rice_item2);
-			request.setAttribute("rice_item3", rice_item3);
-			request.setAttribute("rice_item4", rice_item4);
+			
+			HttpSession session= request.getSession();
+			
+			session.setAttribute("rice_item1", rice_item1);
+			session.setAttribute("rice_item2", rice_item2);
+			session.setAttribute("rice_item3", rice_item3);
+			session.setAttribute("rice_item4", rice_item4);
 			request.setAttribute("rice_item5", rice_item5);
-			red.forward(request, response);
+
+			
+			response.sendRedirect("Desserts.jsp");
 			
 		}catch(Exception e) {
 			System.out.println(e);
