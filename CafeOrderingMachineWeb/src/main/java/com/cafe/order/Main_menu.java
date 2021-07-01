8f96358e6e787d757821ca7c5bd950f777396092
Sharman Jeurkar <72147825+SharmanJeurkar@users.noreply.github.com>
2021-07-01 10:12:02 +0530

    Update Main_menu.java

--- CafeOrderingMachineWeb/src/main/java/com/cafe/order/Main_menu.java
+++ CafeOrderingMachineWeb/src/main/java/com/cafe/order/Main_menu.java
@@ -17,7 +17,7 @@ import com.cafe.dao.Connector;
 
 
 @WebServlet("/Main_Menu")
-public class Main_Menu extends HttpServlet {
+public class MainMenu extends HttpServlet {
 	private static final long serialVersionUID = 1L;
      
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
